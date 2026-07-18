781c791eaec31c79e13766c660307e5505503d65
karencfisher <karencfisher447@gmail.com>
2026-07-18 13:09:07 -0600

    Fixed bug with CW transmit settings

--- examples/transmit.py
+++ examples/transmit.py
@@ -27,12 +27,12 @@ def main():
             play(message)
         elif response.upper() == '/T':  # toggle side tone
             tone_on = not tone_on
-            CWT.set_settings(tone_on=tone_on)
+            CWT.set_settings(tone_on=tone_on, on_air=on_air)
             out = 'on' if tone_on else 'off'
             print(f'Side tone is {out}')
         elif response.upper() == '/A':
             on_air = not on_air
-            CWT.set_settings(on_air=on_air)
+            CWT.set_settings(on_air=on_air, tone_on=tone_on)
             out = 'On' if on_air else 'Off'
             print(f'{out} the air')
         else:
